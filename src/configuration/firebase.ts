40924d2912d33ce8400e5e3ea2a7d6a96da2e9d9
Jintao Koong <jintaokoong99@gmail.com>
2022-05-29 14:34:51 +0800

    feat: update firebase config method

--- src/configuration/firebase.ts
+++ src/configuration/firebase.ts
@@ -1,10 +1,17 @@
 import dotenv from 'dotenv';
 import * as admin from 'firebase-admin';
+import { getOr } from './envvar';
 
 dotenv.config();
 
 const app = admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-})
+  credential: admin.credential.cert(
+    JSON.parse(
+      Buffer.from(getOr('GOOGLE_CONFIG_BASE64', ''), 'base64').toString(
+        'ascii',
+      ),
+    ),
+  ),
+});
 
 export default app;
